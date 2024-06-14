8c56b18a8a5846e7e9ebb98f4bc26eda3e208ace
Ân Nguyễn <annd.dev22@gmail.com>
2024-06-14 16:47:45 +0700

    Revert "- remove quill editor focus node"
    This reverts commit 6b5d25f457399267102808409b51ca58d81feb7d.

--- lib/src/widgets/quill/quill_controller.dart
+++ lib/src/widgets/quill/quill_controller.dart
@@ -35,6 +35,7 @@ class QuillController extends ChangeNotifier {
       FocusNode? editorFocusNode}) {
     return QuillController(
       configurations: configurations,
+      editorFocusNode: editorFocusNode,
       document: Document(),
       selection: const TextSelection.collapsed(offset: 0),
     );
